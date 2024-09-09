e9cd2b1f66dccda1be559476045b06add24966c1
gru-erick <ermateze@gmail.com>
2024-09-08 22:33:09 -0400

    - Updating home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import Image from "next/image";
 import Description from "@/app/components/home/description/description";
+import Hero from "@/app/components/home/hero/hero";
+import MainProducts from "@/app/components/home/mainProducts/mainProducts";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <h1>HOla mundo</h1>
-      <Description></Description>
+        <Hero></Hero>
+        <MainProducts></MainProducts>
+        <Description></Description>
     </div>
   );
 }
