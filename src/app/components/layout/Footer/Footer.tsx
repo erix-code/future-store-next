0dfaf9fd6ada42dc2676eb4c1bf78ce44e32ef62
gru-erick <ermateze@gmail.com>
2024-09-23 15:44:22 -0400

    - Improving footer.

--- src/app/components/layout/Footer/Footer.tsx
+++ src/app/components/layout/Footer/Footer.tsx
@@ -2,8 +2,12 @@
 export default function Footer() {
     console.log("Footer");
     return (
-        <footer>
-            <p>Future Store</p>
+        <footer className={"flex flex-col w-full h-24 justify-center items-center"}>
+            <div className={"border-black border w-full mb-5 h-2 bg-gradient-to-r from from-indigo-600 to-pink-600"}>
+            </div>
+            <h3 className="text-xl bg-gradient-to-r from-pink-600 to-indigo-600 md:text-3xl  text-transparent bg-clip-text">
+                Future Store
+            </h3>
         </footer>
     )
 }
